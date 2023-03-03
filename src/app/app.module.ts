093fcc9274a6237e94090bda470f5641e38341d5
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2023-03-03 00:09:23 -0500

    chore: Socket.io npm package installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: 'http://localhost:3300', options: {} };
 
 @NgModule({
   declarations: [
@@ -12,7 +15,8 @@ import { NotFoundComponent } from './shared/pages/not-found/not-found.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
